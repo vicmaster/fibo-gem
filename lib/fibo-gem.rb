341c629479d032f4a8b8e4398257b02ad11b7374
Victor Velazquez <victor.velazquez@crowdint.com>
2011-10-16 00:59:35 -0500

    new changes

--- lib/fibo-gem.rb
+++ lib/fibo-gem.rb
@@ -1,14 +1,13 @@
 class Fixnum
     def closest_fibonacci
         a,b = 0,1
-        begin
-            self.times  do
-                a,b = b,a+b
-                if(b>=self)
-                    puts "#{a}"
-                    break
-                end
+        self.times  do
+            a,b = b,a+b
+            if(b>=self)
+                return a
             end
         end
+        result = a
+        puts result
     end
 end
